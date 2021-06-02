1d7bbaa774bf9db69c5717d0aa990e0eb6517d2a
Yongho5580 <Yongho5580@users.noreply.github.com>
2021-06-02 22:19:07 +0900

    completed

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var sayHi = function (name, age, gender) {
+    console.log("Hello " + name + ", you are " + age + ", your gender is " + gender);
+};
+sayHi("Yongho", 23, "MALE");
+//# sourceMappingURL=index.js.map
